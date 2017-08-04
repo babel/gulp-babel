21bf286ab4917ffa1e5423d3d35cec621826431f
Henry Zhu <hi@henryzoo.com>
2017-08-03 20:20:22 -0400

    Babel 7 alpha (#112)

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
-var path = require('path');
-var gutil = require('gulp-util');
-var through = require('through2');
-var applySourceMap = require('vinyl-sourcemaps-apply');
-var objectAssign = require('object-assign');
-var replaceExt = require('replace-ext');
-var babel = require('babel-core');
+const path = require('path');
+const gutil = require('gulp-util');
+const through = require('through2');
+const applySourceMap = require('vinyl-sourcemaps-apply');
+const replaceExt = require('replace-ext');
+const babel = require('babel-core');
 
 function replaceExtension(fp) {
 	return path.extname(fp) ? replaceExt(fp, '.js') : fp;
@@ -26,7 +25,7 @@ module.exports = function (opts) {
 		}
 
 		try {
-			var fileOpts = objectAssign({}, opts, {
+			const fileOpts = Object.assign({}, opts, {
 				filename: file.path,
 				filenameRelative: file.relative,
 				sourceMap: Boolean(file.sourceMap),
@@ -34,19 +33,21 @@ module.exports = function (opts) {
 				sourceMapTarget: file.relative
 			});
 
-			var res = babel.transform(file.contents.toString(), fileOpts);
+			const res = babel.transform(file.contents.toString(), fileOpts);
 
-			if (file.sourceMap && res.map) {
-				res.map.file = replaceExtension(res.map.file);
-				applySourceMap(file, res.map);
+			if (res !== null) {
+				if (file.sourceMap && res.map) {
+					res.map.file = replaceExtension(res.map.file);
+					applySourceMap(file, res.map);
+				}
+
+				if (!res.ignored) {
+					file.contents = new Buffer(res.code); // eslint-disable-line unicorn/no-new-buffer
+					file.path = replaceExtension(file.path);
+				}
+
+				file.babel = res.metadata;
 			}
-
-			if (!res.ignored) {
-				file.contents = new Buffer(res.code);
-				file.path = replaceExtension(file.path);
-			}
-
-			file.babel = res.metadata;
 
 			this.push(file);
 		} catch (err) {
