085c752ee5fabe46dffac94a036844a50c0aa364
Huáng Jùnliàng <jlhwung@gmail.com>
2026-07-07 20:00:33 -0400

    Remove caller feature detection (#231)
    * chore: remove caller feature detection
    
    Fixes #217
    
    * chore: bump transitive deps

--- index.js
+++ index.js
@@ -24,11 +24,6 @@ module.exports = function (opts) {
 			return;
 		}
 
-		if (!supportsCallerOption()) {
-			cb(new PluginError('gulp-babel', '@babel/core@>=7.0.0 is required'));
-			return;
-		}
-
 		const fileOpts = Object.assign({}, opts, {
 			filename: file.path,
 			filenameRelative: file.relative,
@@ -65,26 +60,3 @@ module.exports = function (opts) {
 		);
 	});
 };
-
-// Note: We can remove this eventually, I'm just adding it so that people have
-// a little time to migrate to the newer RCs of @babel/core without getting
-// hard-to-diagnose errors about unknown 'caller' options.
-let supportsCallerOptionFlag;
-function supportsCallerOption() {
-	if (supportsCallerOptionFlag === undefined) {
-		try {
-			// Rather than try to match the Babel version, we just see if it throws
-			// when passed a 'caller' flag, and use that to decide if it is supported.
-			(babel.loadPartialConfigSync || babel.loadPartialConfig)({
-				caller: undefined,
-				babelrc: false,
-				configFile: false
-			});
-			supportsCallerOptionFlag = true;
-		} catch (_) {
-			supportsCallerOptionFlag = false;
-		}
-	}
-
-	return supportsCallerOptionFlag;
-}
